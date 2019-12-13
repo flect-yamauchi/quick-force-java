a54fc7ce4d5d7bafa7e00ef1bc751e1a1f61ebd1
flect-yamauchi <58807191+flect-yamauchi@users.noreply.github.com>
2019-12-13 17:50:52 +0900

    Update Application.java
    query change

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -149,7 +149,7 @@ public class Application extends Controller {
             System.out.println("authInfo.instanceUrl:"+authInfo.instanceUrl);
             CompletionStage<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v34.0/query/")
                     .addHeader("Authorization", "Bearer " + authInfo.accessToken)
-                    .addQueryParameter("q", "SELECT Id, Name, Type, Industry FROM Account")
+                    .addQueryParameter("q", "SELECT Id, Name FROM ccrz__E_Product__c")
                     .get();
 
             return responsePromise.thenCompose(response -> {
