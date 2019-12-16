f0eef30e12015cb4ba59e4c3d0421de68ca544cb
flect-yamauchi <58807191+flect-yamauchi@users.noreply.github.com>
2019-12-16 10:39:42 +0900

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -149,7 +149,7 @@ public class Application extends Controller {
             System.out.println("authInfo.instanceUrl:"+authInfo.instanceUrl);
             CompletionStage<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v47.0/query/")
                     .addHeader("Authorization", "Bearer " + authInfo.accessToken)
-                    .addQueryParameter("q", "SELECT Id, Name FROM ccrz__E_Product__c")
+                    .addQueryParameter("q", "SELECT Id, Name FROM Contact")
                     .get();
 
             return responsePromise.thenCompose(response -> {
